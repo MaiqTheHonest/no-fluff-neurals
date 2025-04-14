ef3f65c43ecd1fd53e28197bc383243a0feba5f3
MaiqTheHonest <rdemka@gmail.com>
2025-04-14 14:03:15 +0100

    idek

---
+++ rustic/target/debug/deps/rustic-171c2b7d3f699360.d
@@ -0,0 +1,5 @@
+C:\Users\Robert\projects\nn\rustic\target\debug\deps\librustic-171c2b7d3f699360.rmeta: src\main.rs
+
+C:\Users\Robert\projects\nn\rustic\target\debug\deps\rustic-171c2b7d3f699360.d: src\main.rs
+
+src\main.rs:
---
+++ rustic/target/debug/deps/rustic-2cdc0594d9e13ca7.d
@@ -0,0 +1,5 @@
+C:\Users\Robert\projects\nn\rustic\target\debug\deps\librustic-2cdc0594d9e13ca7.rmeta: src\main.rs
+
+C:\Users\Robert\projects\nn\rustic\target\debug\deps\rustic-2cdc0594d9e13ca7.d: src\main.rs
+
+src\main.rs:
