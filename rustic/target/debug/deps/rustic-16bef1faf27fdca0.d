b73cf38ebea15ab871b619738e8c4d728441da73
MaiqTheHonest <rdemka@gmail.com>
2025-04-14 12:33:09 +0100

    add python prototype and cargo shell

---
+++ rustic/target/debug/deps/rustic-16bef1faf27fdca0.d
@@ -0,0 +1,5 @@
+C:\Users\Robert\projects\nn\rustic\target\debug\deps\rustic-16bef1faf27fdca0.exe: src\main.rs
+
+C:\Users\Robert\projects\nn\rustic\target\debug\deps\rustic-16bef1faf27fdca0.d: src\main.rs
+
+src\main.rs:
---
+++ rustic/target/debug/rustic.d
@@ -0,0 +1,1 @@
+C:\Users\Robert\projects\nn\rustic\target\debug\rustic.exe: C:\Users\Robert\projects\nn\rustic\src\main.rs
